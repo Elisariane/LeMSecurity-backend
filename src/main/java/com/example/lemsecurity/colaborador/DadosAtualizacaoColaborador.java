478446628a1680d9fe93c994da794ba4748419c3
Elisariane <elisarianeBS@gmail.com>
2023-11-10 15:45:23 -0300

    :recycle: refactor: Organizando imports

--- src/main/java/com/example/lemsecurity/colaborador/DadosAtualizacaoColaborador.java
+++ src/main/java/com/example/lemsecurity/colaborador/DadosAtualizacaoColaborador.java
@@ -1,9 +1,6 @@
 package com.example.lemsecurity.colaborador;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
